f3f73fb1a23d468b92c49f830f9c2d8da7de4f83
twinsmaram-hub <twinsmaram@gmail.com>
2025-11-26 09:17:58 +0300

    add sound for rules

--- js/app.js
+++ js/app.js
@@ -17,7 +17,9 @@ let boycottedBrands = [
     { name: "نتفليكس / Netflix", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTNhrG5bf9nxS54v58Ox_Z9mISmOM9z8Fveg&s" },
     { name: "شاي ليبتون / Lipton Tea", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVFkUlcT-AdLGvwT4MMCJ6qx5whI0fAfDNlg&s" },
     { name: "أوريو / Oreo", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYpJO8Zh2zHfQNQsjKViNQmg-8vW4Ei9AYGw&s" },
-    { name: "طلبات / Talabat", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqcpLqoUPruJWyULn3k3tTvSPYUMKyXuML5g&s" }
+    { name: "طلبات / Talabat (Delivery App)", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqcpLqoUPruJWyULn3k3tTvSPYUMKyXuML5g&s" },
+    { name: "سفن دايز /7Days", image: "https://th.bing.com/th/id/OIP.PWSv6n46ve-graNIP-l1PgAAAA?w=202&h=202&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" },
+
 ];
 
 let alternativeBrands = [
@@ -31,19 +33,19 @@ let alternativeBrands = [
     { name: "باجه / Baja", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShRoK80VWpp0Zcby2j2Gtlye_KuMHevq0fVQ&s" },
     { name: "شاي الربيع / Al Rabie Tea", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4-YMIl9OePG_WzmQk1HazfY0edQjc1OrlA&s" },
     { name: "شاي كبوس / Kaboos Tea", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSab1Jv6Ya-ZP5CCKjQunuwzotlRhI7qpp9XA&s" },
-    { name: "علي / Ali Café", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKohWTqcCrX0rd5m7N7eJySDSKIdtPFh4vBQ&s" },
+    { name: "علي كافيه/ Ali Café", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKohWTqcCrX0rd5m7N7eJySDSKIdtPFh4vBQ&s" },
     { name: "هيمالايا / Himalaya", image: "https://cdn.salla.sa/ejzd/vB5y7gkpRr8UEPChmsTdZlc5SLiyRKCue6PhlxVP.jpg" },
     { name: "داك / Dac", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKxTGy2fiTN5y7Q3-9W6-zau8TAwZNvywVNg&s" },
     { name: "جينتو / Gento", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2tV-tlHmWcGxjo8kf2bB4WK3nF-qZuO9hIg&s" },
     { name: "جاهز / Jahez (Delivery App)", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnrLSofiMtwJ95abi6WHwqcyIPWSksJMk49Q&s" },
     { name: "شاومي / Xiaomi", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ4I794dPVIBQaP3_IE6SRmXkeLn0oaGUPpQ&s" },
-    { name: "بينار / Pinar)", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVdwT8lma7GRfXbn44eUTsKIbB3gpBZqpVhg&s" }, 
+    { name: "بينار / Pinar", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVdwT8lma7GRfXbn44eUTsKIbB3gpBZqpVhg&s" }, 
     { name: "لينوفو / Lenovo", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0QAUO4Xp7SQ7Ta78mDP6pvJwVdmOW6dm-fcXYeiYkxUBsv3zusZYWbd__kNvwfF6NSvA&usqp=CAU" },
     { name: "توشيبا / Toshiba", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB_xV_89bEze0MZswmJGyzp32EK8xsk8PxrQ&s" },
     { name: "شارب / Sharp", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7SrQ7KSnYj9v6OUvGSV-dz9LEULwURwvgZg&s" },
     { name: "العملاق / Al Amalaq", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqfJdVuZOf0Q43bjpLmgiaC3O-ISKkX1chAg&s" },
     { name: "ماكس كلين / Max Clean", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQKi7jJvo373YoUfvSYbhmBj35lFUDHHdgA&s" },
-    { name: "بحر / Bahar (Water)", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpXgNFQkhxPsfVsSGDB9RuoZbRGQIy3kUCYQ&s" },
+    { name: "بحر / Bahar", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpXgNFQkhxPsfVsSGDB9RuoZbRGQIy3kUCYQ&s" },
     { name: "LC Waikiki", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOjJGU6GGdmPYeO8cEviAg1f27Zyt_RiqoHw&s" }
 ];
 
@@ -79,13 +81,18 @@ const openBtnEle = document.querySelector("#open-rule")
 const closeBtnEle = document.querySelector('#close-rule')
 const closeRuleElement=document.querySelector('#close-rule')
 const soundCurrect=document.getElementById('currect')
+const soundrule=document.getElementById('sound-rule')
 const soundWrong=document.getElementById('wrong')
 const bycottsound=document.getElementById('Boycott')
+
 const firstimage=document.getElementById('iby')
 const pupElement=document.getElementById('pup')
+const imgRuleElement=document.getElementById('rule-image')
+
 
 
 /*-------------------------------- Functions --------------------------------*/
+
 //function of pup
 function showPup(){
     pupElement.classList.add('open')
@@ -93,6 +100,8 @@ function showPup(){
 
 function closePup(){
      pupElement.classList.remove('open')
+     soundrule.pause();
+     soundrule.currenTime=0;
 }
 //function to show pages
 function showPage(num){
@@ -207,13 +216,14 @@ function returnfun (){
 
 /*----------------------------- Event Listeners -----------------------------*/
 
- starterElement.addEventListener('click',sart)
+  starterElement.addEventListener('click',sart)
   yesBotElement.addEventListener('click',YES)
   noBotElement.addEventListener('click',NO)
   returnElement.addEventListener('click',returnfun)
   firstimage.addEventListener('click',() => {  bycottsound.play()})
   openBtnEle.addEventListener('click',showPup)
   closeBtnEle.addEventListener('click',closePup)
+  imgRuleElement.addEventListener('click',() => {  soundrule.play()})
 
 
  
